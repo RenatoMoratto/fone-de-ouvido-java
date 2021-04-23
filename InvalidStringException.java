4b61c7b4c0c0b4495dd42dd59de1a73b5bf8b153
Renato Willyan Moratto Filho <80411445+RenatoMoratto@users.noreply.github.com>
2021-04-23 18:20:13 -0300

    Update InvalidStringException.java

--- InvalidStringException.java
+++ InvalidStringException.java
@@ -1,7 +1,22 @@
 //Nome: Renato Willyan Moratto Filho - RA.: 2266270
 
 public class InvalidStringException extends Exception {
-    public void invString(){
+
+    Leitura leitura = new Leitura();
+
+    public void invString() { // Mensagem de erro
         System.out.println("Resposta invalida! Tente novamente.");
+    }
+
+    public ComCabo corrigeTipoEntrada(ComCabo cabo) { // Tipo de entrada
+        try {
+            cabo.setTipoEntrada(leitura.entDados("\nTipo de Entrada...: "));
+
+        } catch (InvalidStringException ise) {
+            ise.invString();
+            System.out.println("Tipos de entrada existentes: p1, p2, p3, p10 e USB");
+            ise.corrigeTipoEntrada(cabo);
+        }
+        return cabo;
     }
 }
